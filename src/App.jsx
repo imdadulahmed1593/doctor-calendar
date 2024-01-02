3295dcde6a99f82d87c5d6e98a47af5fe23eb8e0
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2024-01-02 14:08:15 +0600

    feat: useContext setup && month change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { getMonth } from './util';
 import CalendarHeader from './components/CalendarHeader';
 import Sidebar from './components/Sidebar';
 import Month from './components/Month';
-// import GlobalContext from './context/GlobalContext';
+import GlobalContext from './context/GlobalContext';
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
-  // const { monthIndex, showEventModal } = useContext(GlobalContext);
+  const { monthIndex, showEventModal } = useContext(GlobalContext);
 
-  // useEffect(() => {
-  //   setCurrentMonth(getMonth(monthIndex));
-  // }, [monthIndex]);
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
 
   return (
     <React.Fragment>
